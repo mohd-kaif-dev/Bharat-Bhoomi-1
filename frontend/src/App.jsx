520d39242dc83fc1d235a4d9d35c6a6ba2e8e32c
kaif-lucifer <mohdkaif5243human@gmail.com>
2025-06-06 11:47:39 +0530

    Navigate Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Components/Common/Navbar";
 import Home from "./Pages/Home";
 import Footer from "./Components/Common/Footer";
